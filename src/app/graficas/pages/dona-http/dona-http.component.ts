7f2f558c68e4b24dcd020dbdf1e58c13f94075ab
masset151 <masset.ruiz151@gmail.com@gmail.com>
2021-05-05 13:22:28 +0200

    Mostrar la informacion en la grafica

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { Color, Label, MultiDataSet } from 'ng2-charts';
 import { Observable } from 'rxjs';
 import { GraficasService } from '../../services/graficas.service';
 
@@ -8,12 +10,39 @@ import { GraficasService } from '../../services/graficas.service';
   styleUrls: ['./dona-http.component.css']
 })
 export class DonaHttpComponent implements OnInit {
-  
+  public doughnutChartLabels: Label[] //= [//'Partido Popular', 'Mas Madrid', 'PSOE','Vox','Podemos'];
+  public doughnutChartData: MultiDataSet = [
+    //[64, 24, 24,13,10],
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
+
+  public colors: Color[] = [
+
+    {
+      backgroundColor:[
+        "#23AFFC",
+        "#12F075",
+        "#FA0B00",
+        "#44FA04",
+        "#A420EB"
+
+
+      ]
+    }
+    
+  ]
 
   constructor(private graficasService:GraficasService) { }
 
   ngOnInit(): void {
-    this.graficasService.getUsuariosRedesSociales().subscribe(data => console.log(data))
+    this.graficasService.getUsuariosRedesSociales().subscribe(data =>{
+      const labels = Object.keys(data)
+      const values = Object.values(data)
+     this.doughnutChartLabels = labels;
+     this.doughnutChartData.push(values)
+    })
   }
+
+  
 
 }
