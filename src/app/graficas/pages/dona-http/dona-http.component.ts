db8cd6dc9a85b1716f0bea112c066df7b4d203eb
masset151 <masset.ruiz151@gmail.com@gmail.com>
2021-05-05 13:40:09 +0200

    Graficas en Angular completado

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -35,12 +35,15 @@ export class DonaHttpComponent implements OnInit {
   constructor(private graficasService:GraficasService) { }
 
   ngOnInit(): void {
-    this.graficasService.getUsuariosRedesSociales().subscribe(data =>{
-      const labels = Object.keys(data)
-      const values = Object.values(data)
-     this.doughnutChartLabels = labels;
-     this.doughnutChartData.push(values)
-    })
+    
+    this.graficasService.getUsuariosRedesSocialesDonaData().subscribe(({labels,values}) => {
+      this.doughnutChartLabels = labels;
+      this.doughnutChartData.push(values)
+    }
+      
+      
+      
+      )
   }
 
   
